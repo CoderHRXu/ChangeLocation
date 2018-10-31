4ee34e6ef24d4324e37bfc0af3b4d06fef276473
xuhaoran <xuhaoran@fclassroom.com>
2018-10-31 14:55:54 +0800

    initialize

---
+++ ChangeLocation/ChangeLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChangeLocation
+//
+//  Created by haoran on 2018/10/31.
+//  Copyright © 2018年 haoran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
